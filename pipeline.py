414f5dd9c25b2e27db6fe01ebaea1b45208560c6
joseph9513 <a20191373@pucp.edu.pe>
2025-07-04 12:45:41 -0500

    Update pipeline

--- pipeline.py
+++ pipeline.py
@@ -4,7 +4,7 @@ from src.data.upload_s3 import upload_processed_data
 from src.models.train_model import train_model
 from src.models.upload_s3 import upload_model
 
-BUCKET = "ta2-temasD-bucket-789159"
+BUCKET = "ta2-temasd-bucket-789159"
 
 if __name__ == "__main__":
     raw_local = "data/raw/raw_dataset.csv"
